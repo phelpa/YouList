6b8bc4088a8ec3b558eb2051a7f574166c98a282
Plinio Helpa <pliniohelpa@gmail.com>
2021-01-06 09:52:55 -0300

    chore: update yarnlock

--- src/adapters/index.tsx
+++ src/adapters/index.tsx
@@ -3,5 +3,4 @@ import MyDialog from './MyDialog/index';
 import MyDialogTitle from './MyDialogTitle';
 import MyDialogContent from './MyDialogTitle';
 import MyDialogActions from './MyDialogActions';
-
 export { MyButton, MyDialog, MyDialogTitle, MyDialogContent, MyDialogActions };
