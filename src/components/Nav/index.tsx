bcccc873b60e2041ac4cb764a38ea951128d54ba
phelpa <pliniohelpa@gmail.com>
2021-10-14 20:41:33 -0300

    styles: remove tachyons from nav

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-// import styles from './styles.module.css'
+
+import styles from './styles.module.css'
+
 const Nav = () => {
   return (
-    <header className="bg-white black-80 tc pv4">
-      <a className="bg-black-80 ba b--black dib pa3 w2 h2 br-100" href="/"></a>
-      <h1 className="mt2 mb0 baskerville i fw1 f1">YouList</h1>
-      <h2 className="mt2 mb0 f6 fw4 ttu tracked">
+    <header className={styles.header}>
+      <a className={styles.black_circle} href="/"></a>
+      <h1 className={styles.app_name}>YouList</h1>
+      <h2 className={styles.title_text}>
         your own customized video list, create your annotations
       </h2>
     </header>
